c534bfc3554adc0c896366a52efbfa29d79e77de
Vatsal Jain <vatsal@Vatsals-MacBook-Air.local>
2026-04-28 20:13:20 +0530

    ACAO Issue Solved

--- app/main.py
+++ app/main.py
@@ -121,14 +121,20 @@ app = FastAPI(
     version="1.0.0"
 )
 
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173",  
+    "https://solarcurtailmentoptimizer.vercel.app", 
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
 )
-
 @app.get("/")
 async def read_root():
     return {"status": "Prediction Backend is online"}
